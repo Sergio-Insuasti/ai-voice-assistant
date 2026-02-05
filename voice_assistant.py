35152455b09715b12f9da3506fbcdd3baf9967d6
WallOfNoise <z5338374@ad.unsw.edu.au>
2026-02-05 21:02:15 +1100

    Attempting to implement asynchronous functionality to decrease conversational buffer. Implementing loop to continue conversation

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,23 +1,58 @@
-import speech_recognition as sr, pyttsx3
+import asyncio
+import speech_recognition as sr
+import pyttsx3
 from google import genai
 from secrets import INSTRUCTIONS, GOOGLE_API
 from utils.text_cleaner import clean_response
+from concurrent.futures import ThreadPoolExecutor
 
 client = genai.Client(GOOGLE_API)
-r = sr.Recognizer()
+recogniser = sr.Recognizer()
 tts = pyttsx3.init()
+executor = ThreadPoolExecutor()
 
-with sr.Microphone() as mic:
-    print("Speak...")
-    spoken = r.recognize_google(r.listen(mic))
+def listen_blocking():
+    pass
+
+def speak_blocking():
+    pass
+
+def llm_blocking():
+    pass
+
+
+async def listen():
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(executor, listen_blocking)
+
+async def ask_llm(prompt: str):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(executor, llm_blocking, prompt)
+
+async def speak(text: str):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(executor, speak_blocking, text)
+
+async def conversation_loop():
+    print("Your Assistant is now ready. Speak into the microphone")
     
-resp = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f"""{INSTRUCTIONS}{spoken}
-    """
-)
+    while True:
+        try:
+            spoken = await listen()
+            
+            if spoken.lower() in {"exit", "quit", "leave"}:
+                tts.say("Thank you Sergio! Have a great day!")
+                break
+                
+            reply = await ask_llm(spoken)
+            
+            asyncio.create_task(speak(reply))
+        except sr.UnknownValueError:
+            tts.say("I'm sorry, but I was not able to understand you. Please try again")
+        except Exception as e:
+            continue
 
-reply = clean_response(resp.text)
-print("Assistant :", reply)
-tts.say(reply)
-tts.runAndWait()
+
+
+if __name__ == "__main__":
+    asyncio.run(conversation_loop())
